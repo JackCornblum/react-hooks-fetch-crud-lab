a1b947c009d3fd9d9d232047ea723779a761b3ad
jackcornblum <jcornblum@gmail.com>
2021-06-13 19:08:59 -0400

    finished all but UPDATE method

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,12 @@ import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questionData, setQuestionData] = useState([])
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm /> : <QuestionList setPage={setPage} questionData={questionData} setData={setQuestionData}/>}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,49 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import QuestionForm from "./QuestionForm";
 
-function QuestionList() {
+
+
+function QuestionList({questionData, setData, setPage}) {
+  
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then(resp => resp.json())
+    .then(data => {
+      setData(data)
+    })
+  }, [handleDelete])
+
+  function handleDelete(e){
+    console.log(e.target.id)
+    fetch(`http://localhost:4000/questions/${e.target.id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type":"application/json"
+      },
+    })
+  }
+
+  function displayQuestionForm(e){
+    setPage("Form")
+    console.log(e)
+    // return <QuestionForm data={}/>
+  }
+
+  let displayData = questionData.map(obj => {
+    return (
+      <>
+        <li>{obj.prompt} <button id={obj.id} onClick={handleDelete}>Delete Question</button><button onClick={displayQuestionForm}>Question Form</button></li>
+      
+      </>
+    )
+  })
+
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{displayData}</ul>
     </section>
   );
 }
